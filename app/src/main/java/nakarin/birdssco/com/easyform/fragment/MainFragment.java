cdb905c238601e17d2e0440d66fb8ea3e1429df8
birdssco <backhot99@gmail.com>
2017-09-17 10:51:17 +0700

    add fragment ok

---
+++ app/src/main/java/nakarin/birdssco/com/easyform/fragment/MainFragment.java
@@ -0,0 +1,24 @@
+package nakarin.birdssco.com.easyform.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import nakarin.birdssco.com.easyform.R;
+
+/**
+ * Created by opticman on 17-Sep-17.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}  //Main class
